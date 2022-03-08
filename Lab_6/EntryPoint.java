c3a73375f016217a7499a4545a0169d06a84b588
Vanessa Hanna <99267778+VanessaHanna05@users.noreply.github.com>
2022-03-08 18:03:41 +0200

    Add files via upload

---
+++ Lab_6/EntryPoint.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class EntryPoint {
+public static void main(String[] args) {
+	
+	BasicWeek S =  new BasicWeek();
+	S.printDays();
+		
+	
+	AdvancedWeek S1 =  new AdvancedWeek();
+	S1.printDays();
+}
+}
