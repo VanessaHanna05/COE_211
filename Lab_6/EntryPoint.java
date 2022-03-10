d7b3c30b26bd90ea51aef991a301f9c3a0ebd4df
Vanessa Hanna <99267778+VanessaHanna05@users.noreply.github.com>
2022-03-10 18:07:50 +0200

    Add files via upload

---
+++ Lab_6/EntryPoint.java
@@ -0,0 +1,56 @@
+package Assignment;
+import java.util.Scanner;
+public class EntryPoint {
+public static void main(String[] args) {
+
+	
+	Scanner scan =  new Scanner(System.in);
+	
+	while (true) {
+	System.out.print("Which service would you like to use?\r\n"
+			+ "[1]: Basic week visualizer\r\n"
+			+ "[2]: Advanced week visualizer\r\n"
+			+ "[3]: Basic calculator\r\n"
+			+ "[4]: Employee repertoire\r\n"
+			+ "");
+	int service =  scan.nextInt();
+	
+	while (service!=1 && service!=2 && service!= 3 && service !=4) {
+		System.out.println("Please make sure you pick a number between 1 and 4");
+		service = scan.nextInt();
+		
+	}
+	
+	switch (service) {
+	case 1: 
+		BasicWeek S =  new BasicWeek();
+	S.printDays();
+		break;
+	
+	
+	case 2:
+		AdvancedWeek S1 = new AdvancedWeek();
+	S1.printDays();	
+	break;
+	
+	case 3:
+		Calculator Calc = new Calculator();
+	break;
+	
+	
+	
+	case 4:
+		Employee Emp = new Employee();
+	System.out.println(Emp.formation());	;
+		break;
+}
+
+System.out.println("Would you like to perform another operation? (0/1)");
+	int n  = scan.nextInt();
+	if( n == 0) {
+		break;
+	}
+	
+	}}
+	
+}
