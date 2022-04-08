4dd87b8310f7691d8f61e70207f56e90f38e51fc
Vanessa Hanna <99267778+VanessaHanna05@users.noreply.github.com>
2022-04-08 17:30:29 +0300

    Add files via upload

---
+++ lab_8/Finances.java
@@ -0,0 +1,54 @@
+package lab_8;
+import java.util.Scanner;
+import java.io.*;
+public class Finances {
+public static void main(String [] args) throws IOException{
+
+		String option;
+	
+		Scanner scan =  new Scanner(System.in);	
+		File newfile = new File("expenses.txt");
+		Scanner scanFromFile = new Scanner(newfile);
+		FileWriter file = new FileWriter(newfile, true);
+
+		do {
+	
+			System.out.print("Input your name: ");
+			String name  =  scan.nextLine();
+	
+			System.out.print("What did you purchase? ");
+			String item =  scan.nextLine();
+	
+			System.out.print("How much did you purchase? (in USD) ");
+			double payment = scan.nextDouble();
+			scan.nextLine();
+			
+			String str  = name + " purchased "+ item + " for " + payment + " USD Dollars.";
+			file.write(str+"\n");
+	
+			System.out.println("would you like to log another purchase? (y/n)");
+			option = scan.nextLine();
+	
+			}
+		
+			while(option.equalsIgnoreCase("y"));
+		
+			file.close();
+		
+		//Scanning from the file
+		
+			System.out.print("Would you like to read a summary of your purchases? ");
+			String sum = scan.next();
+			
+	
+			if(sum.equalsIgnoreCase("y")) {
+				while(scanFromFile.hasNext()){
+				
+					String line =  scanFromFile.nextLine();
+					System.out.println(line);
+			}
+			
+			
+		}
+}	
+}
